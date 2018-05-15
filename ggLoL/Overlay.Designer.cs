d43938ecc5b346444f7c639b1ead76b9b4842c04
JCases <37308043+JCases@users.noreply.github.com>
2018-05-15 15:15:29 +0200

    Create Form Overlay

---
+++ ggLoL/Overlay.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace ggLoL
+{
+    partial class Overlay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Overlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Overlay";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Sizable = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ggLoL - In-Game";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ ggLoL/Overlay.cs
@@ -0,0 +1,14 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using MaterialSkin.Animations;
+
+namespace ggLoL
+{
+    public partial class Overlay : MaterialForm
+    {
+        public Overlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
